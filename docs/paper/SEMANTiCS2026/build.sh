f90c3692bc4272149d4c7ccc9be90505c7e48daf
Maxim Van de Wynckel <maxim.vandewynckel@vlaanderen.be>
2026-07-22 12:00:47 +0200

    chore: release candidate

--- docs/paper/SEMANTiCS2026/build.sh
+++ docs/paper/SEMANTiCS2026/build.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
+set -e
 cd "$(dirname "$0")"
-rm -f main.pdf main.aux main.bbl main.blg main.log main.fls main.fdb_latexmk
-pdflatex -interaction=nonstopmode -shell-escape main.tex
-bibtex main
-pdflatex -interaction=nonstopmode -shell-escape main.tex
-pdflatex -interaction=nonstopmode -shell-escape main.tex
+rm -f main.pdf main.aux main.bbl main.blg main.log main.fls main.fdb_latexmk build.log
+
+run_step() {
+	"$@" >> build.log 2>&1 || {
+		cat build.log
+		exit 1
+	}
+}
+
+run_step pdflatex -interaction=nonstopmode -shell-escape main.tex
+run_step bibtex main
+run_step pdflatex -interaction=nonstopmode -shell-escape main.tex
+run_step pdflatex -interaction=nonstopmode -shell-escape main.tex
+
+if grep -E '(^!|LaTeX Warning|Package .* Warning|Class .* Warning|Citation .* undefined|Reference .* undefined|undefined references|Emergency stop|Fatal error)' main.log; then
+	exit 1
+fi
+
+if grep -E '(Warning--|^I couldn|There (was|were) [1-9][0-9]* warning)' main.blg; then
+	exit 1
+fi
+
+echo "Build completed cleanly."
