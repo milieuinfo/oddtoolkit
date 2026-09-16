e5df3f4973845ee6682aa2c480c57ab2db74e4e8
jenkins-systeemgebruiker <noreply-jenkins@omgeving.vlaanderen.be>
2026-09-16 10:24:42 +0000

    docs: deploy from jenkins-Publiek-oddtoolkit-main-41

---
+++ assets/chunks/swimlanesDiagram-ULZ7WXOC.5trs9_7O.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.BwLUA7RM.js";import{_ as a}from"../app.Cig2X5zr.js";import"./chunk-5VM5RSS4.6h16gUfD.js";import"./chunk-XXDRQBXY.CqfeELWg.js";import"./chunk-KBJHAD2P.DGM1oaFl.js";import"./chunk-2GRJ4B5K.4EGlonNd.js";import"./channel.DKq4jkAj.js";import"./framework.rfIdp5fP.js";import"./theme.DZpSgiIH.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
